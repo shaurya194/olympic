0d350fc7ee3548d92d5476799bde3670e2dbe1ac
shaurya194 <94612214+shaurya194@users.noreply.github.com>
2021-11-28 11:26:50 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,32 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "blue";
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 2;
+ctx.arc(250, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2;
+ctx.arc(300, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 2;
+ctx.arc(350, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 2;
+ctx.arc(400, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
